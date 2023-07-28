fd6fa53a3e96a9d8f065c3ea56d7192b022860c3
Eric Jumba <jumbaeric@gmail.com>
2023-07-28 15:51:14 +0300

    user controller complete

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\RiderController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\RidersController;
 use App\Http\Controllers\LocationController;
 use App\Http\Controllers\DashboardController;
 
@@ -29,16 +29,16 @@ Route::group(['middleware' => 'auth'], function () {
     Route::post('/location', [LocationController::class, 'store'])->name('location.store');
     Route::post('/location/fetch-address', [LocationController::class, 'fetchAddress'])->name('location.address');
 
-    Route::get('/rider', [RiderController::class, 'index'])->name('rider.index');
-    Route::post('/rider/get-location', [RiderController::class, 'getLocation'])->name('rider.location');
-    Route::post('/rider/get-route', [RiderController::class, 'getRoute'])->name('rider.route');
+    Route::get('/rider', [RidersController::class, 'index'])->name('rider.index');
+    Route::post('/rider/get-location', [RidersController::class, 'getLocation'])->name('rider.location');
+    Route::post('/rider/get-route', [RidersController::class, 'getRoute'])->name('rider.route');
 
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-    Route::post('/users', [UserController::class, 'store'])->name('users.store');
-    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::get('/users', [UsersController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UsersController::class, 'create'])->name('users.create');
+    Route::post('/users', [UsersController::class, 'store'])->name('users.store');
+    Route::get('/users/{user}/edit', [UsersController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{user}', [UsersController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}', [UsersController::class, 'destroy'])->name('users.destroy');
 });
 
 Auth::routes();
